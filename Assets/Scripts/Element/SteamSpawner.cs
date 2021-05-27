420f067f0bad735e375ca651530db62f0a365ff6
Leningradets <41923218+Leningradets@users.noreply.github.com>
2021-05-27 13:29:36 +0300

    FX Added

---
+++ Assets/Scripts/Element/SteamSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteamSpawner : MonoBehaviour
+{
+    [SerializeField] private SteamFX _steamFX;
+
+    private Element _element;
+
+    private void Awake()
+    {
+        _element = GetComponentInParent<Element>();   
+    }
+
+    private void OnEnable()
+    {
+        _element.DataChanged += Spawn;
+    }
+
+    private void OnDisable()
+    {
+        _element.DataChanged -= Spawn;
+    }
+
+    public void Spawn(ElementData data)
+    {
+        Instantiate(_steamFX, transform.position, Quaternion.identity, transform);
+    }
+}
